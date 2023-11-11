64eae07c017cc888fb4aa5e3e0e2fc2a6e685ee6
omanar <omanar@student.1337.ma>
2023-11-11 06:00:23 +0100

    CRUD update

--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -1,1 +1,28 @@
-export class User {}
+import { User } from "@prisma/client";
+import { ApiProperty } from "@nestjs/swagger";
+
+export class UserEntity implements User {
+	@ApiProperty()
+	id: string;
+
+	@ApiProperty()
+	name: string;
+
+	@ApiProperty()
+	email: string;
+
+	@ApiProperty()
+	username: string;
+
+	@ApiProperty()
+	password: string;
+
+	@ApiProperty()
+	level: number;
+
+	@ApiProperty()
+	createdAt: Date;
+
+	@ApiProperty()
+	updatedAt: Date;
+}
