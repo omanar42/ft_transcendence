73f7b5ce5019785f567e8ba67de62081c89e9d7a
mrobaii-1337 <mrmedrobaii@gmail.com>
2023-12-14 03:47:03 +0100

    Chat added

--- frontend/src/Components/NavBar.tsx
+++ frontend/src/Components/NavBar.tsx
@@ -6,7 +6,7 @@ import Welcome from "./Pages/Welcome/Welcome";
 
 function NavBar() {
   return (
-    <div className="">
+    <div className="mb-10">
       <div className="max-w-140 fix   ml-auto mr-auto p-10 text-3xl text-white h-40 flex justify-between items-center">
         <img className="h-10" src={Logo} />
 
@@ -14,7 +14,7 @@ function NavBar() {
           <ul className="flex gap-40">
            <li><Link to="/login">Log in </Link></li> 
            <li><Link to="/home">Home</Link></li> 
-           <li><Link to="/welcom">Chat</Link></li> 
+           <li><Link to="/chat">Chat</Link></li> 
 
           </ul>
         </nav>
---
+++ frontend/src/Components/Pages/Chat/ChatList.tsx
@@ -0,0 +1,139 @@
+import Avatar from "./assets/avatar.jpeg";
+import './Chatlist.css';
+
+const avatars = [Avatar, Avatar, Avatar, Avatar, Avatar, Avatar];
+const chats = [
+  {
+    avatar: Avatar,
+    username: "mrobaii",
+    message: "Anchdk n72ik a balaty",
+    time: "9.00 am",
+  },
+  {
+    avatar: Avatar,
+    username: "omanar",
+    message: "Andoz eandk lyoum",
+    time: "10.00 am",
+  },
+  {
+    avatar: Avatar,
+    username: "yessad",
+    message: "Match nadi a sat",
+    time: "6.00 am",
+  },
+  {
+    avatar: Avatar,
+    username: "yassin",
+    message: "galo liya khsarto ?",
+    time: "12.00 am",
+  },
+  {
+    avatar: Avatar,
+    username: "ilyas",
+    message: "Anchdk n72ik a balaty",
+    time: "9.00 am",
+  },
+  {
+    avatar: Avatar,
+    username: "mrobaii",
+    message: "Anchdk n72ik a balaty",
+    time: "9.00 am",
+  },
+  {
+    avatar: Avatar,
+    username: "mrobaii",
+    message: "Anchdk n72ik a balaty",
+    time: "9.00 am",
+  },
+  {
+    avatar: Avatar,
+    username: "mrobaii",
+    message: "Anchdk n72ik a balaty",
+    time: "9.00 am",
+  },
+  {
+    avatar: Avatar,
+    username: "mrobaii",
+    message: "Anchdk n72ik a balaty",
+    time: "9.00 am",
+  },
+  {
+    avatar: Avatar,
+    username: "mrobaii",
+    message: "Anchdk n72ik a balaty",
+    time: "9.00 am",
+  },
+  {
+    avatar: Avatar,
+    username: "mrobaii",
+    message: "Anchdk n72ik a balaty",
+    time: "9.00 am",
+  },
+  {
+    avatar: Avatar,
+    username: "mrobaii",
+    message: "Anchdk n72ik a balaty",
+    time: "9.00 am",
+  },
+];
+interface ListAvatars {
+  avatar: string;
+}
+
+function ListAvatars({ avatar }: ListAvatars) {
+  return (
+    <li>
+      <img className="rounded-full" src={avatar} alt="avatar" />
+    </li>
+  );
+}
+
+interface ListConversations {
+    avatar: string,
+    username: string,
+    message: string,
+    time: string, 
+}
+
+function ListConversations({ avatar, username, message, time }:ListConversations) {
+  return (
+    <li className="flex items-center gap-5 mb-6">
+      <img className="h-[5rem] rounded-full" src={avatar} alt="avatar" />
+      <div className="overflow-hidden">
+        <h1 className="text-2xl pb-2 font-extrabold">{username}</h1>
+        <p className="text-xl">{message}</p>
+      </div>
+      <span className="text-xl font-semibold">{time}</span>
+    </li>
+  );
+}
+function ChatList() {
+  return (
+    <div className="col-span-1 flex flex-col items-center gap-5 overflow-hidden">
+      <div className=" border-2 border-white border-opacity-20 rounded-lg flex flex-col items-center gap-5 pt-4 pb-4">
+        <input
+          className="w-11/12 h-[2.5rem] rounded-full pl-10 text-black outline-none"
+          type="text"
+          placeholder="search"
+        />
+        <ul className="flex gap-5 w-11/12">
+          {avatars.map((avatar) => (
+            <ListAvatars avatar={avatar} />
+          ))}
+        </ul>
+      </div>
+      <ul className="p-4 scroll-container flex w-full flex-col overflow-auto">
+        {chats.map((conv) => (
+          <ListConversations
+            avatar={conv.avatar}
+            message={conv.message}
+            time={conv.time}
+            username={conv.username}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ChatList;
