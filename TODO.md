e83b70cfd85c22de7b21a637f690efd036b802a2
Oussama Manar <manarsama01@gmail.com>
2023-12-15 19:13:15 +0100

    redirections after login

--- TODO.md
+++ TODO.md
@@ -6,13 +6,32 @@ Project Description
 
 ### Todo
 
-- [ ] prisma command before run docker  
+- [ ] Matching players  
+- [ ] Match History  
+- [ ] server-side validation for forms and any user  
+- [ ] SQL injections  
 
 ### In Progress
 
-- [ ] Docker  
+- [ ] Protectec 2fa in cookies  
+- [ ] status (online, offline, in a game, and so forth)  
 
 ### Done ✓
 
+- [x] Docker  
 - [x] node:lts-ailpine  
+- [x] prisma command before run docker  
+- [x] Two-Factor Auth  
+- [x] Settings  
+- [x] Update profile  
+- [x] upload an avatar  
+- [x] Stats (such as: wins and losses, ladder level, achievements, and so forth)  
+- [x] Profile Endpoints  
+- [x] Update Username  
+- [x] Remove Friend  
+- [x] Add Friend  
+- [x] Block Friend  
+- [x] Friends Database  
+- [x] refresh tokens  
+- [x] JWT  
 
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -95,27 +95,34 @@ export class AuthService {
     }
   }
 
-	async login(req, res) {
-    const user = req.user;
-    if (!user) {
-      throw new BadRequestException('Unauthenticated');
+    async login(req, res) {
+      let redirectUrl: string;
+      const user = req.user;
+      if (!user) {
+        throw new BadRequestException('Unauthenticated');
+      }
+
+      let found = await this.usersService.findOneByEmail(user.email);
+      redirectUrl = process.env.FRONTEND_URL + '/home';
+    
+      if (!found) {
+        found = await this.registerUser(user);
+        redirectUrl = process.env.FRONTEND_URL + '/welcome';
+      }
+
+      const tokens = await this.getTokens(found.oauthId, found.email);
+      await this.updateRtHash(found.oauthId, tokens.refresh_token);
+
+      res.cookie('access_token', tokens.access_token, { httpOnly: true, secure : true});
+      res.cookie('refresh_token', tokens.refresh_token, { httpOnly: true, secure : true});
+
+      this.usersService.setStatus(found.oauthId, Status["ONLINE"]);
+      
+      if (found.twoFactor)
+        redirectUrl = process.env.FRONTEND_URL + '/two-factor';
+
+      return res.status(HttpStatus.OK).json(redirectUrl);
     }
-
-    let found = await this.usersService.findOneByEmail(user.email);
-
-    if (!found)
-      found = await this.registerUser(user);
-
-    const tokens = await this.getTokens(found.oauthId, found.email);
-    await this.updateRtHash(found.oauthId, tokens.refresh_token);
-
-    res.cookie('access_token', tokens.access_token, { httpOnly: true, secure : true});
-    res.cookie('refresh_token', tokens.refresh_token, { httpOnly: true, secure : true});
-
-    this.usersService.setStatus(found.oauthId, Status["ONLINE"]);
-		
-    return res.status(HttpStatus.OK).json(tokens);
-	}
 
   async logout(oauthId: string) {
 		await this.prisma.user.updateMany({
