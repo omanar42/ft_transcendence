b76c0b30cf759f41e7ac727ff237551600063f7e
meslall <meslalla90@gmail.com>
2023-12-29 19:07:01 +0100

    ADD_get_rooms_http

--- backend/src/chat/chat.controller.ts
+++ backend/src/chat/chat.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { Response, Request } from 'express';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { ChatService } from './chat.service';
+import { AtGuard } from 'src/auth/guards';
+
 
 @Controller('chat')
-export class ChatController {}
+@UseGuards(AtGuard)
+export class ChatController {
+  constructor(
+    private readonly chatService: ChatService,
+    private prisma: PrismaService,
+  ) {}
+  @Get('Rooms')
+  async getRooms(@Req() req) {
+    console.log(req.user.sub);
+    return await this.chatService.getRooms();
+  }
+  @Post('CreateRoom')
+  async createRoom() {
+  }
+}
