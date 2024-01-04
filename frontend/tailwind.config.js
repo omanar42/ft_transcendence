5c5f78f21d9516107b250fb229baad0be854090f
th3saint <mrmedrobaii@gmail.com>
2024-01-04 21:25:23 +0100

    sf

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -35,6 +35,9 @@ export default {
         'dark-100': '#2b2c26',
         'dark-200': '#2b2c26',
         'dark-300': '#42433e',
+        'pink-100': '#953e91',
+        'pink-200': '#4b1f49',
+        'blue-100': '#1C00CBB2'
       },
       letterSpacing: {
         '1': '0em',
