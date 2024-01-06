2a62717c0809845a83d77d1cdbc187932574eb71
omanar <omanar@student.1337.ma>
2024-01-06 17:05:15 +0100

    Game logic on backend

--- backend/src/game/player.ts
+++ backend/src/game/player.ts
@@ -1,31 +1,57 @@
 export class Player {
   id: number;
-  paddlePosition: number;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  color: string;
   score: number;
 
   constructor(id) {
     this.id = id;
-    this.paddlePosition = 0;
+    this.y = 700 / 2 - 100 / 2;
+    this.width = 16;
+    this.height = 128;
     this.score = 0;
+    if (id === 1) {
+      this.x = 4;
+      this.color = '#41a5fc';
+    } else {
+      this.x = 1300 - 5 - 16;
+      this.color = '#f600d4';
+    }
   }
 
   updatePaddlePosition(position) {
-    this.paddlePosition = position;
+    this.y = position;
   }
 
-  updateScore(score) {
-    this.score = score;
+  updateScore() {
+    this.score++;
   }
 
   reset() {
-    this.paddlePosition = 0;
+    this.y = 700 / 2 - 100 / 2;
+    this.width = 16;
+    this.height = 128;
     this.score = 0;
+    if (this.id === 1) {
+      this.x = 4;
+      this.color = '#41a5fc';
+    } else {
+      this.x = 1300 - 5 - 16;
+      this.color = '#f600d4';
+    }
   }
 
   toJSON() {
     return {
       id: this.id,
-      paddlePosition: this.paddlePosition,
+      x: this.x,
+      y: this.y,
+      width: this.width,
+      height: this.height,
+      color: this.color,
       score: this.score,
     };
   }
