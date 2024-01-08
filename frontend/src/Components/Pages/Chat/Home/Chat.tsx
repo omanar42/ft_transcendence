1af146e122e3f04a9e3e6ab2a96566f9e4354908
th3saint <mrmedrobaii@gmail.com>
2024-01-08 18:47:58 +0100

    2fa On its way

--- frontend/src/Components/Pages/Chat/Home/Chat.tsx
+++ frontend/src/Components/Pages/Chat/Home/Chat.tsx
@@ -2,17 +2,27 @@ import ChatList from "./ChatList";
 import MessageInput from "./MessageInput";
 import { useEffect, useState } from "react";
 import UserInfo from "./UserInfo";
+import { Room } from "../Rooms/RoomList";
+import axios from "axios";
 
 function Chat() {
-  const [currentChatuser, setCurentUser] = useState("");
-  
-  const showChatuser = (user: any) => {
-    setCurentUser(user);
-  };
-
+  const [usersRoom, setUsersRoom] = useState<Room[]>([]);
+  useEffect(() => {
+    const fetchRooms = async () => {
+      try {
+        const response = await axios.get("http://127.0.0.1:3000/chat/dms", {
+          withCredentials: true,
+        });
+        setUsersRoom(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchRooms();
+  }, [usersRoom]);
   return (
     <div className="bg-dark-100 h-[62rem] grid grid-cols-5 gap-x-2 mt-4">
-      <ChatList chatUser={showChatuser} />
+      <ChatList chatUser={usersRoom} />
       <MessageInput />
       <UserInfo />
     </div>
